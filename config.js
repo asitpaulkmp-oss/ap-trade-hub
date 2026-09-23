c2ba801a44cbb54b258f813d8c95108665bb4597
asitpaulkmp-oss <asitpaul.kmp@gmail.com>
2026-09-23 17:42:54 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+// AP TRADE HUB CSV CATALOGUE CONFIG
+// Paste your Google Sheets "Published to web" CSV URL here.
+// Example: https://docs.google.com/spreadsheets/d/e/XXXXX/pub?output=csv
+const CATALOG_CSV_URL = "";
